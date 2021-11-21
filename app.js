3f44ca9f6156cb7184bc7af9f26657fc2ed87be7
Shakib40 <shakibjilani@gmail.com>
2021-11-21 21:46:37 +0530

    without Patch

--- app.js
+++ app.js
@@ -1,48 +1,52 @@
 const express = require('express');
-const users = require('./users_data.json');
+const users = require('./books.json');
 
 const app = express();
 
 app.use(express.json() );
 
+// this will return all the users
 app.get("/" , (req,res) =>{
     return res.send( {users} );
 });
 
-app.post("/" , (req,res) =>{
+
+// pass an user to it and it will append it to the end of the users and return it
+app.post("/books" , (req,res) =>{
     const newUser = [...users , req.body ];
     res.send(newUser);
 });
 
-// app.patch("/:id/:email" , (req,res) =>{
+// this will return user with a specific id
+app.get("/books/:id/" , (req,res) =>{
+    res.send(req.params.id);
+});
+
+// app.patch("/books/:id/:email" , (req,res) =>{
 //     console.log(req.params.id);
 //     console.log(req.params.email);
 //     res.send("Post 1");
 // });
 
-app.patch("/:email" , (req,res) =>{
-    const newUser = users.map( (user) =>{
-        if(req.params.email === user.email){
-            if(req?.body?.first_name) user.first_name = req.body.first_name;
-            if(req?.body?.last_name) user.last_name = req.body.last_name;
-            if(req?.body?.email) user.email = req.body.email;
-            if(req?.body?.gender) user.gender = req.body.gender;
-        //    return req.body;
-        }
-        return user;
-    });
-    res.send(newUser);
-});
+// app.patch("/books/:id" , (req,res) =>{
+//     const newUser = users.map( (user) =>{
+//         if(req.params.id === user.id){
+//             if(req?.body?.first_name) user.first_name = req.body.first_name;
+//             if(req?.body?.last_name) user.last_name = req.body.last_name;
+//             if(req?.body?.email) user.email = req.body.email;
+//             if(req?.body?.gender) user.gender = req.body.gender;
+//         //    return req.body;
+//         }
+//         return user;
+//     });
+//     res.send(newUser);
+// });
 
-app.delete("/:email" , (req,res) => {
+app.delete("/books/:id" , (req,res) => {
     const newUser = users.filter( (user) => user.email !== req.params.email);
     res.send(newUser);
 });
 
-app.get("/:email", (req,res) => {
-    const newUser = users.filter( (user) => user.email === req.params.email);   
-    res.json(newUser);
-});
 
 app.listen(2345, function() {
     console.log("Listening on Port 2345");   
